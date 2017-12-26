4108da54ab3719bfdfc6f9d787be50082fb7cfa2
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-26 15:12:37 -0600

    10 fetch video list from youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyClyqgSWPcYWyQNOdPhwio2N7uoRLEDKOg';
-// Create a new component.
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
+
+
+
+// Class Component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos:[]};
+        
+        YTSearch({key:API_KEY,term:'surfboards'},videoList => {
+            this.setState({videos:videoList});
+        })
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )    
+    }
 }
 
 // Take this component's generated HTML and put it on the page (DOM)
