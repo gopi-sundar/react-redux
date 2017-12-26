811fad110b54ae820b137cd8d9491d28c18d0c78
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-26 11:03:19 -0600

    3 Render search_bar.js from inside index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
+const API_KEY = 'AIzaSyClyqgSWPcYWyQNOdPhwio2N7uoRLEDKOg';
 // Create a new component.
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
 }
 
 // Take this component's generated HTML and put it on the page (DOM)
